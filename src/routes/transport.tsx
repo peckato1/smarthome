peckato1/smarthome

commit 2cb8a3947ef4f00cd45e086202e9d3195dd1540c

transport: Add Kosmonosy,nemocnice and Mlada Boleslav,aut.st

src/routes/transport.tsx
@@ -17,6 +17,13 @@ const boards = [
     { icon: <FilterBadge type="tram"  direction="up"   />, func: (d: model.Departure) => +d.route.type === 0 && d.stop.platform_code === 'B'},
   ]},
   { name: "Nádraží Veleslavín", count: 7, filters: [] },
+  { name: "Kosmonosy,nemocnice", count: 5, filters: [
+    { icon: <FilterBadge type="bus" direction="down" />, func: (d: model.Departure) => d.stop.platform_code === 'B', active: true},
+    { icon: <FilterBadge type="bus" direction="up"   />, func: (d: model.Departure) => d.stop.platform_code === 'A' },
+  ]},
+  { name: "Mladá Boleslav,aut.st.", count: 5, filters: [
+    { icon: <FilterBadge type="bus" direction="down" />, func: (d: model.Departure) => ['2', '3', '7', '8', '11', '12', '20'].includes(d.stop.platform_code) },
+  ]},
 ]
 
 export default function TransportRoute() {

commit 40450702190b3cf371af64c6a4a991fefcdf2689

transport: make Prague filter at Ml.B.,aut.st. active by default
Fixes: 2cb8a39 ("transport: Add Kosmonosy,nemocnice and Mlada Boleslav,aut.st")

src/routes/transport.tsx
@@ -22,7 +22,7 @@ const boards = [
     { icon: <FilterBadge type="bus" direction="up"   />, func: (d: model.Departure) => d.stop.platform_code === 'A' },
   ]},
   { name: "Mladá Boleslav,aut.st.", count: 5, filters: [
-    { icon: <FilterBadge type="bus" direction="down" />, func: (d: model.Departure) => ['2', '3', '7', '8', '11', '12', '20'].includes(d.stop.platform_code) },
+    { icon: <FilterBadge type="bus" direction="down" />, func: (d: model.Departure) => ['2', '3', '7', '8', '11', '12', '20'].includes(d.stop.platform_code), active: true },
   ]},
 ]
 